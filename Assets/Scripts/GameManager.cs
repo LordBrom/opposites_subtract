7906449415e6c02ff36a2ffe0350d1ec67bf8412
Nate Mills <mills.nate@gmail.com>
2021-12-03 09:17:40 -0800

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	#region Inspector Assignments
+
+	#endregion
+	#region Variables
+
+	#endregion
+
+	#region Unity Methods
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+	#endregion
+}
